a0908994ee30fa56e811ee21f2d2566cd15dcf33
tiziaco <tiziano.iaco@gmail.com>
2023-12-07 23:36:07 +0100

    modify user services

--- server/main/services/user_service.py
+++ server/main/services/user_service.py
@@ -28,12 +28,37 @@ def save_new_user(data):
         }
         return response_object, 409
 
-
 def get_all_users():
     return User.query.all()
 
 def get_a_user(public_id):
-    return User.query.filter_by(public_id=public_id).first()
+    user = User.query.filter_by(public_id=public_id).first()
+    if user:
+        return user
+    else:
+        response_object = {
+            'status': 'fail',
+            'message': 'User not found',
+        }
+        return response_object, 409
+
+def delete_user(public_id):
+    user_to_delete = User.query.get(public_id)
+
+    if user_to_delete:
+        db.session.delete(user_to_delete)
+        db.session.commit()
+        response_object = {
+            'status': 'success',
+            'message': 'User deleted.'
+        }
+        return response_object, 201
+    else:
+        response_object = {
+            'status': 'fail',
+            'message': 'User not found.',
+        }
+        return response_object, 409
 
 def save_changes(data):
     db.session.add(data)
