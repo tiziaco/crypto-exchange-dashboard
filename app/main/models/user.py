1d718f6c51e0fedd59038b351954e6589828712a
tiziaco <tiziano.iaco@gmail.com>
2023-12-11 19:35:17 +0100

    add email validation

--- app/main/models/user.py
+++ app/main/models/user.py
@@ -1,3 +1,7 @@
+import re
+import uuid
+from datetime import datetime
+
 from .. import db
 from app.main.models.portfolio import Portfolio
 
@@ -10,7 +14,20 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(60), nullable=False)
     registered_on = db.Column(db.DateTime, nullable=False)
+    is_active = db.Column(db.Boolean, default=True, nullable=False)
     portfolios = db.relationship('Portfolio', backref='user', lazy=True)
+
+    def __init__(self, username, email, password):
+        self.public_id = str(uuid.uuid4())
+        self.username = username
+        self.email = self.validate_email(email)
+        self.password = password
+        self.registered_on = datetime.utcnow()
+
+    def validate_email(self, email):
+        if not re.match(r"[^@]+@[^@]+\.[^@]+", email):
+            raise ValueError("Invalid email format")
+        return email
 
     # @property
     # def password(self):
